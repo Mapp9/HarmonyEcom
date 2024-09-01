ceaba42eff3833061dba478f319c5650a9ea05b7
Mapp9 <mpp.adso@gmail.com>
2024-09-01 16:52:36 -0500

    add service class User, Category, Product

---
+++ backend/src/main/java/com/harmonydealer/ecommerce/backend/application/UserService.java
@@ -0,0 +1,19 @@
+package com.harmonydealer.ecommerce.backend.application;
+
+import com.harmonydealer.ecommerce.backend.domain.model.User;
+import com.harmonydealer.ecommerce.backend.domain.port.IUserRepository;
+
+public class UserService {
+    private final IUserRepository iUserRepository;
+
+    public UserService(IUserRepository iUserRepository) {
+        this.iUserRepository = iUserRepository;
+    }
+
+    public User save(User user){
+        return this.iUserRepository.save(user);
+    }
+    public User findById(Integer id){
+        return this.iUserRepository.findById(id);
+    }
+}
