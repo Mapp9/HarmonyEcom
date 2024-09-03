5a2a16b4bbd09b73f381b3351553cd5ffd1fa736
Mapp9 <mpp.adso@gmail.com>
2024-09-03 14:29:43 -0500

    add category controller

--- backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/config/BeanConfiguration.java
+++ backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/config/BeanConfiguration.java
@@ -1,6 +1,8 @@
 package com.harmonydealer.ecommerce.backend.infrastructure.config;
 
+import com.harmonydealer.ecommerce.backend.application.CategoryService;
 import com.harmonydealer.ecommerce.backend.application.UserService;
+import com.harmonydealer.ecommerce.backend.domain.port.ICategoryRepository;
 import com.harmonydealer.ecommerce.backend.domain.port.IUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +13,10 @@ public class BeanConfiguration {
     @Bean
     public UserService userService(IUserRepository iUserRepository){
         return new UserService(iUserRepository);
+    }
+
+    @Bean
+    public CategoryService categoryService(ICategoryRepository iCategoryRepository){
+        return new CategoryService(iCategoryRepository);
     }
 }
