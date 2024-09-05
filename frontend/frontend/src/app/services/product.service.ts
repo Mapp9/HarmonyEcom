59c2101813a1e8644f6705abc66a1798b0c87f2b
Mapp9 <mpp.adso@gmail.com>
2024-09-04 22:56:09 -0500

    Add CRUD methods to ProductService

--- frontend/frontend/src/app/services/product.service.ts
+++ frontend/frontend/src/app/services/product.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../common/product';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  // URL api products
+  private apiUrl: string = "http://localhost:8085/api/v1/admin/products";
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+// Get all products
+  getProducts():Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.apiUrl);
+  }
+// Create a product
+  createProduct(formData:any):Observable<any>{
+    return this.httpClient.post<Product>(this.apiUrl, formData);
+  }
+  // Delete a product
+  deleteProductById(id:number):Observable<any>{
+    return this.httpClient.delete(this.apiUrl + "/" + id);
+  }
+  // Get a product by id
+  getProductById(id:number):Observable<Product>{
+    return this.httpClient.get<Product>(this.apiUrl + "/" + id);
+    }
 }
