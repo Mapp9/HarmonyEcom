1570a1ba66d8f212d1601270bb84d97aef1c4e53
Mapp9 <mpp.adso@gmail.com>
2024-09-12 15:18:28 -0500

    Update save method in ProductService to handle product image upload

--- backend/src/main/java/com/harmonydealer/ecommerce/backend/application/ProductService.java
+++ backend/src/main/java/com/harmonydealer/ecommerce/backend/application/ProductService.java
@@ -3,13 +3,26 @@ package com.harmonydealer.ecommerce.backend.application;
 import com.harmonydealer.ecommerce.backend.domain.model.Product;
 import com.harmonydealer.ecommerce.backend.domain.port.IProductRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @AllArgsConstructor
 public class ProductService {
     private final IProductRepository iProductRepository;
+    private final UploadFile uploadFile;
 
 
-    public Product save(Product product){
+    public Product save(Product product, MultipartFile multipartFile) throws IOException {
+        if (product.getId()!=0){
+            if (multipartFile==null){
+                product.setUrlImage(product.getUrlImage());
+            }else {
+                product.setUrlImage(uploadFile.upload(multipartFile));
+            }
+        }else {
+            product.setUrlImage(uploadFile.upload(multipartFile));
+        }
         return this.iProductRepository.save(product);
     }
     public Iterable<Product> findAll(){
