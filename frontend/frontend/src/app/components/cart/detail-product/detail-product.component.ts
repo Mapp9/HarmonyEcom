737edbf33db43e78f73481f4a396548b3e356609
Mapp9 <mpp.adso@gmail.com>
2024-09-13 04:43:08 -0500

    Implement add product to cart

--- frontend/frontend/src/app/components/cart/detail-product/detail-product.component.ts
+++ frontend/frontend/src/app/components/cart/detail-product/detail-product.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-detail-product',
   templateUrl: './detail-product.component.html',
   styleUrls: ['./detail-product.component.css']
 })
-export class DetailProductComponent {
+export class DetailProductComponent implements OnInit {
+  id:number = 0;
+  name:string = '';
+  description:string = '';
+  price:number = 0;
+  urlImage:string = '';
+  quantity:number = 0;
 
+
+  ngOnInit(): void {
+    this.getProductById();
+  }
+
+  constructor(private productService:ProductService, private activatedRoute:ActivatedRoute, private cartService:CartService){}
+
+  getProductById(){
+    this.activatedRoute.params.subscribe(
+      p => {
+        let id = p['id'];
+        if(id){
+          this.productService.getProductById(id).subscribe(
+            data => {
+              this.name = data.name;
+              this.description = data.description;
+              this.urlImage = data.urlImage;
+              this.price = data.price;
+            }
+          );
+        }
+      }
+    );
+  }
+
+  addCart(id:number){
+    console.log('id product: ', id);
+    console.log('name product: ', this.name);
+    console.log('price product: ', this.price);
+    console.log('quantity product: ', this.quantity);
+  }
 }
