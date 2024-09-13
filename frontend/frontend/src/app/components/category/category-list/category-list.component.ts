cb3c3d8a3c484f615385f43cbfa0b4393b6f4d6b
Mapp9 <mpp.adso@gmail.com>
2024-09-13 03:25:01 -0500

    Implement delete category method

--- frontend/frontend/src/app/components/category/category-list/category-list.component.ts
+++ frontend/frontend/src/app/components/category/category-list/category-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Category } from 'src/app/common/category';
 import { CategoryService } from 'src/app/services/category.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-category-list',
@@ -22,6 +23,34 @@ export class CategoryListComponent implements OnInit {
     this.categoryService.getCategoryList().subscribe(
       data => this.categories = data
     )
+  }
+
+  deleteCategoryById(id:number){
+    console.log('El ID de la categoria antes de eliminar es: '+id);
+
+    Swal.fire({
+      title: "¿Está seguro que desea eliminar el registro?",
+      text: "",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Eliminar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.categoryService.deleteCategoryById(id).subscribe(
+          ()=> this.listCategories()
+        );
+        Swal.fire({
+          title: "¡Categorías!",
+          text: "Categoría eliminada correctamente.",
+          icon: "success"
+        });
+      }
+    });
+
+    
   }
 
 }
