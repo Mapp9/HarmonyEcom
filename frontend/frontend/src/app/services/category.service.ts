99d3e3027950861c9bd45167e3e8212a99f43d70
Mapp9 <mpp.adso@gmail.com>
2024-09-13 01:53:53 -0500

    Implement CRUD methods

--- frontend/frontend/src/app/services/category.service.ts
+++ frontend/frontend/src/app/services/category.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Category } from '../common/category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,19 @@ export class CategoryService {
   private apiUrl: string = 'http://localhost:8085/api/v1/admin/categories'
   constructor(private http:HttpClient) { }
 
-  getCategoryList(){
-
+  getCategoryList():Observable<Category[]>{
+    return this.http.get<Category[]>(this.apiUrl);
   }
 
-  createCategory(category:Category){
-
+  createCategory(category:Category):Observable<Category>{
+    return this.http.post<Category>(this.apiUrl,category);
   }
 
-  deleteCategory(id:number){
-
+  deleteCategoryById(id:number):Observable<any>{
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
-  getCategoryById(id:number){
-    
+  getCategoryById(id:number):Observable<Category>{
+    return this.http.get<Category>(`${this.apiUrl}/${id}`);
   }
 }
