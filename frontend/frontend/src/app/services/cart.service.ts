68ec150d79ed015b2dba9e4b7a41eff69f9bab83
Mapp9 <mpp.adso@gmail.com>
2024-09-13 04:19:09 -0500

    Create methods to add, delete, and add up the total price

--- frontend/frontend/src/app/services/cart.service.ts
+++ frontend/frontend/src/app/services/cart.service.ts
@@ -13,18 +13,38 @@ export class CartService {
   constructor() { }
 
   addItemCart(itemCart: ItemCart){
-
+    this.items.set(itemCart.productId, itemCart);
   }
 
   deleteItemCart(productId:number){
-
+    this.items.delete(productId);
+    this.items.forEach(
+      (valor, clave) => {
+        console.log('Esta es la clave y su valor: '+clave, valor);
+      } 
+    );
   }
 
   totalCart(){
-
+    let totalCart: number = 0;
+    this.items.forEach(
+      (item, clave)=>{
+        totalCart+=item.getTotalPriceItem();
+        console.log(`Item: ${clave}, Precio: ${item.getTotalPriceItem()}, Total parcial: ${totalCart}`);
+      }
+    );
+    console.log(`Total final del carrito: ${totalCart}`);
+    return totalCart;
   }
 
   convertToListFromMap(){
-    
+    this.itemList.splice(0,this.itemList.length);
+    this.items.forEach(
+      (item, clave)=>{
+        this.itemList.push(item);
+      }
+    );
+
+    return this.itemList;
   }
 }
