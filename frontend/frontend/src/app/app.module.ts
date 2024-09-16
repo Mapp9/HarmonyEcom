16eb2f8db0e2bf2893dd2bcb2feb16751ba8c9ff
Mapp9 <mpp.adso@gmail.com>
2024-09-15 22:42:26 -0500

    Implement add product to cart visuality

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -14,7 +14,8 @@ import { CategoryListComponent } from './components/category/category-list/categ
 import { CategoryAddComponent } from './components/category/category-add/category-add.component';
 import { DetailProductComponent } from './components/cart/detail-product/detail-product.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { SumaryOrderComponent } from './components/orders/sumary-order/sumary-order.component'
 
 const routes : Routes = [
   {path: '', component: HomeComponent},
@@ -24,7 +25,8 @@ const routes : Routes = [
   {path: 'admin/category', component: CategoryListComponent },
   {path: 'admin/category/add', component: CategoryAddComponent},
   {path: 'admin/category/update/:id', component: CategoryAddComponent},
-  {path: 'cart/detailproduct/:id', component: DetailProductComponent}
+  {path: 'cart/detailproduct/:id', component: DetailProductComponent},
+  {path: 'cart/sumary', component: SumaryOrderComponent}
 ];
 
 @NgModule({
@@ -37,7 +39,8 @@ const routes : Routes = [
     CategoryListComponent,
     CategoryAddComponent,
     DetailProductComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    SumaryOrderComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/frontend/src/app/components/orders/sumary-order/sumary-order.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ItemCart } from 'src/app/common/item-cart';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-sumary-order',
+  templateUrl: './sumary-order.component.html',
+  styleUrls: ['./sumary-order.component.css']
+})
+export class SumaryOrderComponent implements OnInit {
+
+  items : ItemCart [] = [];
+  totalCart: number = 0;
+  firstName : string = '';
+  lastName : string = '';
+  email : string = '';
+  addres : string = '';
+
+  constructor(private cartService:CartService){}
+
+  ngOnInit(): void {
+    this.items = this.cartService.convertToListFromMap();
+    this.totalCart = this.cartService.totalCart();
+  }
+
+
+  deleteItemCart(productId:number){
+this.cartService.deleteItemCart(productId);
+this.items = this.cartService.convertToListFromMap();
+this.totalCart = this.cartService.totalCart();
+
+  }
+
+}
