6ce8c9657822ccecbf8cf4f68d7e99c394f306d6
Mapp9 <mpp.adso@gmail.com>
2024-09-20 21:09:48 -0500

    Fix and update permission for home page

--- backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/admin/products/**").hasRole("ADMIN")
                         .requestMatchers("/api/v1/orders/**").hasRole("USER")
                         .requestMatchers("/api/v1/payments/**").hasRole("USER")
+                        .requestMatchers("/images/**").permitAll()
                         .requestMatchers("/api/v1/home/**").permitAll()
                         .requestMatchers("/api/v1/security/**").permitAll().anyRequest().authenticated()
 
--- backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/rest/HomeController.java
+++ backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/rest/HomeController.java
@@ -4,12 +4,14 @@ package com.harmonydealer.ecommerce.backend.infrastructure.rest;
 import com.harmonydealer.ecommerce.backend.application.ProductService;
 import com.harmonydealer.ecommerce.backend.domain.model.Product;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/home")
+@CrossOrigin(origins = "http://localhost:4200")
 public class HomeController {
     private final ProductService productService;
 
