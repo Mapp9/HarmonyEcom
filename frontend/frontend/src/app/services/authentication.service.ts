e3294921963d45850748c82d35961f648446070c
Mapp9 <mpp.adso@gmail.com>
2024-09-20 21:26:47 -0500

    Add register and auth services

---
+++ frontend/frontend/src/app/services/authentication.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../common/user';
+import { Observable } from 'rxjs';
+import { Userdto } from '../common/userdto';
+import { Jwtclient } from '../common/jwtclient';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private apiUrl : string = 'http://localhost:8085/api/v1/security';
+
+  constructor(private httpClient : HttpClient) { }
+
+
+  registrer(user : User):Observable<User>{
+    return this.httpClient.post<User>(this.apiUrl+"/registrer", user);
+  }
+
+  login(userDto:Userdto):Observable<Jwtclient>{
+    return this.httpClient.post<Jwtclient>(this.apiUrl+"/login", userDto)
+  }
+}
