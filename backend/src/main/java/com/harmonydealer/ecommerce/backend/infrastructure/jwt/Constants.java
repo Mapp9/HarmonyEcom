a5102982fddb19a347cc37a9ff9631078a892870
Mapp9 <mpp.adso@gmail.com>
2024-09-26 03:33:14 -0500

    Modify token expiration time

--- backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/jwt/Constants.java
+++ backend/src/main/java/com/harmonydealer/ecommerce/backend/infrastructure/jwt/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 
     public static final String SUPER_SECRET_KEY = "fniZhrPyN7rG4v6H0b45v3DGulvN907STIM4qiOVXhAJMBJCcvZjv8A9CP50s5qqrOKF4vVtZXB";
 
-    public static final long TOKEN_EXPIRATION_TIME = 1500000;
+    public static final long TOKEN_EXPIRATION_TIME = 2500000;
 
     public static Key getSignedKey(String secretKey){
         byte [] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
